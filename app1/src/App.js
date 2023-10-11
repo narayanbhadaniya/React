63b22a0031733daee78ec6a7005b6203ebbf1809
NarayanBhadaniya <narayanbhadaniya68@email.com>
2023-10-11 15:51:49 +0530

    Right Mount Unmount

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,113 +1,109 @@
-import React from "react";
-import "./App.css";
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react';
 
-class InnerComponent extends React.Component{
-  render(){
-    return(
-      <div>
-        Inner Component is Here
-      </div>
-    )
+class InnerComponent extends React.Component
+{
+  render()
+  {
+    return <div>Inner component is here</div>
   }
-  componentWillUnmount(){
-    console.log("Unmounting Inner Component")
+
+  componentDidMount()
+  {
+    console.log("####Mounting InnerComponent####")
+  }
+
+  componentWillUnmount()
+  {
+    console.log("****Unmounting InnerComponent****")
   }
 
 }
 
-
-class App extends React.Component {
-  state = { counter: 10 , importnnerComponent : <InnerComponent/> };
-  constructor(props) {
+class App extends React.Component
+{
+  state = {counter:0, innerComponent:<InnerComponent/>}
+  constructor(props)
+  {
     super(props)
-    console.log("I am inside constructor")
+    console.log("I am inside Constructor")
   }
 
-  
-  counterIncrement = ()=>{
+  static getDerivedStateFromProps()
+  {
+    console.log("I am inside getDerivedStateFromProps")
+    return true
+  }
+
+  shouldComponentUpdate()
+  {
+    console.log("Inside shouldComponentUpdate")
+    return true
+  }
+
+  incrementCounter = () => 
+  {
+    //updating state on function call
     this.setState({counter:this.state.counter+1})
-    this.setState({innerComponent :<div>Unmounted</div>})
-   
-  }
-  static getDerivedStateFromProps(){
-    console.log("I am inside Derived State")
-    return true
-  }
-
-  shouldComponentUpdate(){
-    console.log("I am inside Should Component Update")
-    return true;
-
-  }
-  //overridden render function
-  render() {
-    console.log("I am inside Render")
-    console.log(this.props)
-    return (
-      <div>
-        <button onClick={this.counterIncrement}>Click Me</button>
-        <br/>
-        Counter :{this.state.counter}
-        <br/>
-        <div>Component :{this.innerComponent}</div>
-        
-        
-      </div>
-    );
-  }
-  getSnapshotBeforeUpdate(prevProps,prevState){
-    console.log("I am inside Get Snapshot Before Update")
-    console.log("Previous State : "+ prevState)
-    console.log("Previous Props : "+ prevProps)
-    console.log("Props : "+ this.props)
-    console.log("State : "+ this.state)
-    return true
-
-  }
-
-  componentDidMount(){
-    console.log("I am inside ComponentDid Mount")
-    return true
+    if(this.state.counter % 2 == 0)
+    {
+      this.setState({innerComponent:<InnerComponent/>})
+    }
+    else
+    {
+      this.setState({innerComponent:<div>Unmounted</div>})
+    }
     
   }
+
+  // Overridden render function
+  render()
+  {
+    console.log("Inside the render method")
+    console.log(this.props)
+    return (
+        <div>
+          <button onClick={this.incrementCounter}>Click me</button>
+          <br/>
+          Counter:{this.state.counter}
+          <br/>
+          <div>{this.state.innerComponent}</div>
+        </div>
+    )
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState)
+  {
+    console.log("Inside getSnapshotBeforeUpdate")
+    console.log("Previous Prop:")
+    console.log(prevProps)
+    console.log("Previous State:")
+    console.log(prevState)
+    console.log("--------------------")
+    console.log("Updated Prop:")
+    console.log(this.props)
+    console.log("Update State:")
+    console.log(this.state)
+    return true
+  }
+
+  componentDidMount()
+  {
+    console.log("I am inside componentDidMount")
+  }
 }
 
-// function myComponent() {
-//   return <h1>Hello My Component</h1>;
-// }
-
-// class MyClassComponent extends React.Component {
-//   render() {
-//     return <h1>Class Component</h1>;
-//   }
-// }
-
-// function App(props) {
-//   console.log(props.color);
-//   console.log(props.size);
-//   console.log(props.clickEvent);
+// function App(props) 
+// {
+//   console.log(props.color)
+//   console.log(props.size)
+//   console.log(props.clickEvent)
 //   return (
 //     <div className="App">
-//       <button onClick={props.clickEvent} >Click Me</button>
+//       <button onClick={props.clickEvent}>Click me</button>
 //     </div>
 //   );
 // }
 
-//const classComponent = new MyClassComponent();
-
-//const myComponent = () =>{
-//return <h1>Hello</h1>
-//}
-//return myComponent();
-
-//return classComponent.render();
-
-//return React.createElement(
-// "div",
-// null,
-// React.createElement("h1", null, "This is Heading")
-//);
-//}
-
 export default App;
-
