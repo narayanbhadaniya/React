20d7439cd1c9cd54aa0f2fd2b395efcc9db36e9a
NarayanBhadaniya <narayanbhadaniya68@email.com>
2023-10-25 14:53:30 +0530

    Todo add using CallBack Funtion

--- app1/src/App.js
+++ app1/src/App.js
@@ -1,109 +1,33 @@
-//import logo from './logo.svg';
-import './App.css';
-import React from 'react';
+import React, { useState } from "react";
 
-class InnerComponent extends React.Component
-{
-  render()
-  {
-    return <div>Inner component is here</div>
-  }
+function App(props) {
+  const [todos, setTodo] = useState([]);
 
-  componentDidMount()
-  {
-    console.log("####Mounting InnerComponent####")
-  }
-
-  componentWillUnmount()
-  {
-    console.log("****Unmounting InnerComponent****")
-  }
-
-}
-
-class App extends React.Component
-{
-  state = {counter:0, innerComponent:<InnerComponent/>}
-  constructor(props)
-  {
-    super(props)
-    console.log("I am inside Constructor")
-  }
-
-  static getDerivedStateFromProps()
-  {
-    console.log("I am inside getDerivedStateFromProps")
-    return true
-  }
-
-  shouldComponentUpdate()
-  {
-    console.log("Inside shouldComponentUpdate")
-    return true
-  }
-
-  incrementCounter = () => 
-  {
-    //updating state on function call
-    this.setState({counter:this.state.counter+1})
-    if(this.state.counter % 2 == 0)
-    {
-      this.setState({innerComponent:<InnerComponent/>})
-    }
-    else
-    {
-      this.setState({innerComponent:<div>Unmounted</div>})
-    }
+  function handleClick(event) {
+    event.preventDefault();
+    let newTodo = document.getElementById("addTodo").value;
+    let newTodoObject = {
+      id: new Date().getTime(),
+      text: newTodo,
+    };
     
+    setTodo([...todos, newTodoObject]);
+    document.getElementById("addTodo").value = "";
   }
 
-  // Overridden render function
-  render()
-  {
-    console.log("Inside the render method")
-    console.log(this.props)
-    return (
-        <div>
-          <button onClick={this.incrementCounter}>Click me</button>
-          <br/>
-          Counter:{this.state.counter}
-          <br/>
-          <div>{this.state.innerComponent}</div>
-        </div>
-    )
-  }
-
-  getSnapshotBeforeUpdate(prevProps, prevState)
-  {
-    console.log("Inside getSnapshotBeforeUpdate")
-    console.log("Previous Prop:")
-    console.log(prevProps)
-    console.log("Previous State:")
-    console.log(prevState)
-    console.log("--------------------")
-    console.log("Updated Prop:")
-    console.log(this.props)
-    console.log("Update State:")
-    console.log(this.state)
-    return true
-  }
-
-  componentDidMount()
-  {
-    console.log("I am inside componentDidMount")
-  }
+  return (
+    <div>
+      <h1>ToDo list</h1>
+      <form onSubmit={handleClick}>
+        <input type="text" id="addTodo" />
+        <button type="submit">Add To-do</button>
+      </form>
+      <div>
+        {todos.map((tempTodo) => {
+          return <div><h1>{tempTodo.text}</h1></div>;
+        })}
+      </div>
+    </div>
+  );
 }
-
-// function App(props) 
-// {
-//   console.log(props.color)
-//   console.log(props.size)
-//   console.log(props.clickEvent)
-//   return (
-//     <div className="App">
-//       <button onClick={props.clickEvent}>Click me</button>
-//     </div>
-//   );
-// }
-
 export default App;
