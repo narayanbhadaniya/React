82de4b09a2aa33b4a679ec544540df21d4b6dea7
Narayan Bhadaniya <narayanbhadaniya68@gmail.com>
2023-11-27 12:10:42 +0530

    Pass from child to parent

--- practice/src/App.js
+++ practice/src/App.js
@@ -6,49 +6,42 @@ import React from 'react';
 // feature branch code
 
 //Child Component
-class InnerComponent extends React.Component
-{
-  constructor(props)
-  {
+class InnerComponent extends React.Component {
+  constructor(props) {
     super(props)
   }
 
-  sendData=()=>
-  {
+  sendData = () => {
     const text = document.getElementById("textMessage").value
-    console.log("Data to send to parent is: "+text)
+    console.log("Data to send to parent is: " + text)
     this.props.parentCallBack(text)
   }
 
-  render()
-  {
+  render() {
     return (
       <div>
-        <input type='Text' placeholder='Enter message for parent' id="textMessage"/>
-        <button onClick={()=>this.sendData()}>Send to parent</button>
+        <input type='Text' placeholder='Enter message for parent' id="textMessage" />
+        <button onClick={() => this.sendData()}>Send to parent</button>
       </div>
     )
   }
 }
 
 //parent component
-class App extends React.Component
-{
-  state = {message:"This is from Parent"}
+class App extends React.Component {
+  state = { message: "This is from Parent" }
 
-  parentCallbackFunction = (text) =>
-  {
-    console.log("Data in parent: "+text)
-    this.setState({message: text})
+  parentCallbackFunction = (text) => {
+    console.log("Data in parent: " + text)
+    this.setState({ message: text })
   }
 
-  render()
-  {
+  render() {
     return (
-    <div>
-      <InnerComponent parentCallBack={this.parentCallbackFunction}/>
-      <div>Data From Child:{this.state.message}</div>
-    </div>
+      <div>
+        <InnerComponent parentCallBack={this.parentCallbackFunction} />
+        <div>Data From Child:{this.state.message}</div>
+      </div>
     )
   }
 }
