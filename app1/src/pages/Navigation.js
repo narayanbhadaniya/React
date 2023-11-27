d51715f426d0529c453dffafcd3ee934898d1fa7
Narayan Bhadaniya <narayanbhadaniya68@gmail.com>
2023-11-27 12:47:15 +0530

    React Routers

---
+++ app1/src/pages/Navigation.js
@@ -0,0 +1,26 @@
+import {Outlet,Link } from "react-router-dom";
+import SignUp from "./Signup";
+
+function Navigation()
+{
+    return (<div>
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/signup">Sign Up</Link>
+                </li>
+                <li>
+                    <Link to="/signin">Sign In</Link>
+                </li>
+                <li>
+                    <Link to="/aboutus">About Us</Link>
+                </li>
+            </ul>
+        </nav>
+        <Outlet />
+    </div>)
+}
+export default Navigation;
